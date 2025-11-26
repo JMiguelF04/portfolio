6dc9d7d104ebea3d3f79c759b048140b2b1f3ef1
Jorge Ferreira <jmferreira0307@gmail.com>
2025-11-26 23:54:29 +0000

    debugging code

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "@/components/SideBar";
 import HeroSection from "@/components/HeroSection";
 import Footer from "@/components/Footer";
 
