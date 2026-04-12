f9871c8dfaa04f99d48d7abc99e3279f88be78e1
Jorge Ferreira <jmferreira0307@gmail.com>
2026-04-12 15:55:58 +0100

    .

--- lib/default-site-data.ts
+++ lib/default-site-data.ts
@@ -7,7 +7,7 @@ export const defaultSiteData: SiteData = {
     headlinePt: "Junior Developer",
     headlineEn: "Junior Developer",
     profileImagePath: "/Profile-Photo.png",
-    cvFilePath: "./CV.ENG.pdf",
+    cvFilePath: "/CV.ENG.pdf",
     locationPt: "Lisboa, Portugal",
     locationEn: "Lisbon, Portugal",
     website: "https://www.jmdaf.dev",
